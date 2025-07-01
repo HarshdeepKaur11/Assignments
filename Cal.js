73f42bd6af6b45a1ba9d1d808285306c1fa472dd
Harshdeepkourx <harschx31@gmail.com>
2025-07-01 17:56:08 +0530

    Javascript

---
+++ Cal.js
@@ -0,0 +1,164 @@
+// let a=prompt("Enter your number to check wheather its negative positive or zero");
+// if (a<0)
+// {
+//     alert("Its Negative")
+// }
+// else if (a>0)
+// {
+//     alert("Number is Positive")
+// }
+// else
+// {
+//     alert("Number is 0")
+// }
+
+
+// let num=prompt("Type your number to check if its even or odd");
+// if (num%2==0)
+// {
+//     alert("It even number")
+// }
+// else {
+//     alert("Its odd")
+// }
+
+// alert("Hey kid!! wanna play a Guessing game");
+// alert("So Enter two numbers to check which is bigger");
+// alert("So let's start the game");
+// let First=prompt("Enter first number");
+// let Second=prompt("Enter second number");
+// if (First>Second)
+// {
+//     alert("Yes kid First numbre is greater : " + " " + First);
+// }
+// else
+// {
+//     alert("Yes Kid Second number is greater : " + " " + Second);
+// }
+
+// alert('Hi Sir/Mam welcome to party room');
+// alert("We need to get know about your id if you are over 18")
+// alert("As might some kids can lie")
+// let age=prompt("Bring ur id and tell us your age");
+// if (age<18)
+// {
+//     alert("We can't let you in" + " " + "🤨");
+// }
+// else
+// {
+//     alert("You can come over Welcome!!" + " " + "🤗");
+// }
+
+
+// alert("Hi buddy! Are you bored?  If yes, then let's play a number guessing game!");
+// alert("I’ve picked a number just for you! 🎯 You just have to guess it, ok?");
+// alert("So let's start!");
+
+// let number = parseInt(prompt("Guess your number 👀"));  
+// let computernumber = 40;
+
+// if (number === computernumber) {
+//     alert("Hurray!! You guessed the number 🎉🎉");
+// } else {
+//     alert("Oops! Batti Gull 💡❌🤣 Try again!");
+// }
+
+
+// let num1 = parseInt(prompt("Enter number 1"));
+// let num2 = parseInt(prompt("Enter number 2"));
+// let operator = prompt("Give your operator (+, -, *, /, %)");
+
+// let result;
+
+// if (operator === "+") {
+//   result = num1 + num2;
+// } else if (operator === "-") {
+//   result = num1 - num2;
+// } else if (operator === "*") {
+//   result = num1 * num2;
+// } else if (operator === "/") {
+//   result = num1 / num2;
+// } else if (operator === "%") {
+//   result = num1 % num2;
+// } else {
+//   result = "Invalid operator 😥";
+// }
+
+// alert("Result: " + result);
+
+
+// alert("Today was your result Annie");
+// alert("What did happen");
+// alert("Tell mumma's the result");
+// let result=prompt("Speak it")
+// alert("Mumma" + "📝")
+// if (result>95)
+// {
+//     alert("Great beta work hard Excellent results" + " " + "😍✨🎀" );
+// }
+// else if (result<95 & result>80)
+// {
+//     alert("I am happy with you but work harder"  + " " + "🥰");
+// }
+// else if (result<80 & result>50)
+// {
+//     alert("I'm very upset"  + " " + "😐");
+// }
+// else if(result<50)
+// {
+//     alert("You are fail!!! Go away" + " " + "😏💀" )
+// }
+// alert("Happy ending");
+
+// alert("Hi!!!!! We want to buy an Animal");
+// alert("What kind of Animal do you want?");
+// alert("We want a Wild Animal");
+// alert("Ok, we have: Squirrel, Rat, Rhinoceros, Hippo, Elephant, Giraffe.");
+// alert("Which one do you want?");
+// alert("We are not sure but we want a Squirrel. We can describe.");
+// alert("Sure! You can describe.");
+
+// let height = parseFloat(prompt("Tell us about the height (in feet):"));
+
+// if (height < 1) {
+//   alert("It's short");
+
+//   let speak = prompt("Can it speak? Type 'yes' or 'no':").toLowerCase();
+
+//   if (speak === "yes") {
+//     alert("Might be a wild red rat squirrel");
+//   } else if (speak === "no") {
+//     alert("Might be a normal red rat");
+//   } else {
+//     alert("That's not a valid answer.");
+//   }
+
+// } else if (height >= 1) {
+//   alert("It's tall");
+
+//   let neck = prompt("Tell us about the neck type: Type 'shortneck' or 'tallneck'").toLowerCase();
+
+//   if (neck === "tallneck") {
+//     alert("Might be a long neck squirrel");
+//   } else if (neck === "shortneck") {
+//     alert("Might be a short neck squirrel");
+
+//     let nose = prompt("Tell us about the nose: Type 'shortnose' or 'longnose'").toLowerCase();
+
+//     if (nose === "longnose") {
+//       alert("Might be a tangles squirrel ");
+//     } else if (nose === "shortnose") {
+
+//       let habitat = prompt("Where does it live? Type 'land' or 'water'").toLowerCase();
+
+//       if (habitat === "land") {
+//         alert("Oh its a land squirrel");
+//       } else if (habitat === "water") {
+//         alert("Oh its a water squirrel ");
+//       }
+
+//     }
+
+//   }
+// }
+
